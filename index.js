75adc42ba8784add13c99d7aaa760978bf01966e
Your Name <you@example.com>
2022-12-13 07:42:06 +0300

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+function getImage(){
+fetch;https;//www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F504332858268228602%2F&psig=AOvVaw3NcmQCZ_sleCmfz33Z_ODb&ust=1670992107649000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOiJxf3g9fsCFQAAAAAdAAAAABAE
+fetch;image.png
+console.log("image.png")
+}
+
+
+
