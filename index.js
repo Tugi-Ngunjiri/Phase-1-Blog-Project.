0169b44b9e13d52812fbd2b29b7248899b366329
Your Name <you@example.com>
2022-12-19 11:59:42 +0300

    add Readme

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 function getImage(){
-fetch;https;//www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F504332858268228602%2F&psig=AOvVaw3NcmQCZ_sleCmfz33Z_ODb&ust=1670992107649000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOiJxf3g9fsCFQAAAAAdAAAAABAE
+fetch;file:///home/tugi/Downloads/undraw_scientist_re_fs04%20(1).svg
 fetch;image.png
 console.log("image.png")
 }
 
+el = document.querySelector('#foo')
+var request = new XMLHttpRequest()
+request.open('GET', 'bar.svg', true)
 
+request.onreadystatechange = function() {
+  if (this.readyState !== 4) return
+  if (this.status !== 200) return
+  el.innerHTML = this.responseText
+}
+request.send()
+// bar.svg is inserted in element with ID 'foo'
 
+fetch('bar.svg').then("...")
+
+fetch('bar.svg')
+    .then(r => r.text())
+    .then(text => {
+        el.innerHTML = text;
+    })
+    .catch(console.error.bind(console));
+
+    
